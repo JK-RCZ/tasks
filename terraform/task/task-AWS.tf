25cfae80acae0d01a484f01b5c168ed0afb8c339
JK-RCZ <jankarchewski@outlook.com>
2023-08-06 19:49:56 +0300

    item 4 almost over

--- terraform/task/task-AWS.tf
+++ terraform/task/task-AWS.tf
@@ -1,51 +1,44 @@
 provider "aws" {
-   region = "us-east-1"
+   region = local.region
 
 }
+
+#------------------------------------------ LOCAL VARIABLES ----------------------------------------
+locals {
+  region = "us-east-1"
+  ssh_key_path = "~/Keys-tokens-etc/"    # path to folder, where private ssh key will be generated
+  ssh_key_name = "aws-key.pem"           # name of private ssh key 
+  local_public_IP = "37.214.57.223"      # public IP of local machine (there is restriction to access
+                                         # instances from this specific IP)
+  vpc_cidr_block = "10.0.0.0/26"         # vpc net cidr
+  public1_cidr_block = "10.0.0.0/28"     # subnet #1 cidr
+  public2_cidr_block = "10.0.0.16/28"    # subnet #2 cidr
+  private_cidr_block = "10.0.0.32/28"    # subnet #3 cidr     
+  ami = "ami-07dff4fe919dee33e"          # ami type
+  instance_type = "t2.micro"             # instance type
+}
+
+#------------------------------------------ INPUT VARIABLES ----------------------------------------
 variable "sudo_pass" {
   type = string
-  description = "Sudo password for some file operations."
-
+  description = "Type sudo password for ssh key file manipulations."
   sensitive = true
 }
-variable "ssh_key_path" {
-  type = string
-  sensitive = false
-  default = "~/Keys-tokens-etc/"
-}
-variable "ssh_key_name" {
-  type = string
-  sensitive = false
-  default = "aws-key.pem"
-}
-variable "local_public_IP" {
-  type = string
-  sensitive = false
-  default = "0.0.0.0"
-}
+
 #------------------------------------------- NULL RESOURCE -----------------------------------------
 resource "null_resource" "set-permissions-666-to-ssh-key" {
 
   provisioner "local-exec" {
     command = <<EOT
-                touch ${var.ssh_key_path}${var.ssh_key_name}
-                echo ${var.sudo_pass} | sudo -S chmod 666 ${var.ssh_key_path}${var.ssh_key_name}
-              EOT
-  }
-}
-
-resource "null_resource" "get-local-public-ip" {
-
-  provisioner "local-exec" {
-    command = <<EOT
-                curl ipconfig.io > ${var.ssh_key_path}local-public.ip
+                touch ${local.ssh_key_path}${local.ssh_key_name}
+                echo ${var.sudo_pass} | sudo -S chmod 666 ${local.ssh_key_path}${local.ssh_key_name}
               EOT
   }
 }
 
 #------------------------------------ CREATING VPC & THREE SUBNETS ---------------------------------
 resource "aws_vpc" "terra-task" {
-  cidr_block       = "10.0.0.0/26"
+  cidr_block       = local.vpc_cidr_block
   instance_tenancy = "default"
 
   tags = {
@@ -55,7 +48,7 @@ resource "aws_vpc" "terra-task" {
 
 resource "aws_subnet" "terra-task-public1" {
   vpc_id     = aws_vpc.terra-task.id
-  cidr_block = "10.0.0.0/28"
+  cidr_block = local.public1_cidr_block
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1a"
 
@@ -65,7 +58,7 @@ resource "aws_subnet" "terra-task-public1" {
 }
 resource "aws_subnet" "terra-task-public2" {
   vpc_id     = aws_vpc.terra-task.id
-  cidr_block = "10.0.0.16/28"
+  cidr_block = local.public2_cidr_block
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1b"
 
@@ -75,7 +68,7 @@ resource "aws_subnet" "terra-task-public2" {
 }
 resource "aws_subnet" "terra-task-private" {
   vpc_id     = aws_vpc.terra-task.id
-  cidr_block = "10.0.0.32/28"
+  cidr_block = local.private_cidr_block
   map_public_ip_on_launch = "false"
   availability_zone = "us-east-1a"
 
@@ -83,7 +76,6 @@ resource "aws_subnet" "terra-task-private" {
     Name = "terra-task-private"
   }
 }
-
 
 #------------------------ CREATING SECURITY GROUP TO ALLOW SSH HTTP HTTPS --------------------------
 resource "aws_security_group" "web-sg" {
@@ -97,21 +89,21 @@ resource "aws_security_group" "web-sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["37.214.57.223/32"]
+    cidr_blocks      = ["${local.local_public_IP}/32"]
   }
   ingress {
     description      = "HTTP"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["37.214.57.223/32"]
+    cidr_blocks      = ["${local.local_public_IP}/32"]
   }
   ingress {
     description      = "HTTPS"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = ["37.214.57.223/32"]
+    cidr_blocks      = ["${local.local_public_IP}/32"]
   }
 
   egress {
@@ -127,11 +119,11 @@ resource "aws_security_group" "web-sg" {
 }
 
 
-#------------------------------------ CREATING AWS INSTANCE ---------------------------------
+#------------------------------------ CREATING AWS INSTANCES ---------------------------------
 resource "aws_instance" "Terra-SUSE1" {
   count = 1
-  ami = "ami-07dff4fe919dee33e"
-  instance_type = "t2.micro"
+  ami = local.ami
+  instance_type = local.instance_type
   subnet_id = aws_subnet.terra-task-public1.id
   associate_public_ip_address = "true"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
@@ -139,12 +131,19 @@ resource "aws_instance" "Terra-SUSE1" {
   tags = {
     Name = "TerraSUSE1"
   }
-  
+  user_data = <<EOF
+  #!/usr/bin/env bash
+  sudo zypper -n refresh
+  sudo zypper install -y nginx
+  sudo systemctl start nginx
+  sudo systemctl enable nginx
+  echo "<h1>Welcome to the Dead End!</h1><br><p>If you see this page, you are lost. Completety. Find your way out, or stay here forever.</p>" > /srv/www/htdocs/index.html
+  EOF
 }
 resource "aws_instance" "Terra-SUSE2" {
   count = 1
-  ami = "ami-07dff4fe919dee33e"
-  instance_type = "t2.micro"
+  ami = local.ami
+  instance_type = local.instance_type
   subnet_id = aws_subnet.terra-task-public2.id
   associate_public_ip_address = "true"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
@@ -155,7 +154,7 @@ resource "aws_instance" "Terra-SUSE2" {
   
 }
 
-#---------------------------------------- CREATING KEY PAIR -------------------------------------
+#---------------------------------------- CREATING SSH KEY PAIR -------------------------------------
 resource "tls_private_key" "task-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -165,10 +164,10 @@ resource "aws_key_pair" "jan" {
   public_key = tls_private_key.task-key.public_key_openssh
   
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.task-key.private_key_pem}' > ${var.ssh_key_path}${var.ssh_key_name}"
+    command = "echo '${tls_private_key.task-key.private_key_pem}' > ${local.ssh_key_path}${local.ssh_key_name}"
   }
   provisioner "local-exec" {
-    command = "chmod 400 ${var.ssh_key_path}${var.ssh_key_name}"
+    command = "chmod 400 ${local.ssh_key_path}${local.ssh_key_name}"
   }
   depends_on = [
         null_resource.set-permissions-666-to-ssh-key
@@ -211,10 +210,10 @@ output "Terra-SUSE2-public-IP" {
   value = aws_instance.Terra-SUSE2.*.public_ip
 }
 output "SSH-key-path" {
-  value = var.ssh_key_path
+  value = local.ssh_key_path
 }
-output "Local-public-IP" {
-  value = file("${var.ssh_key_path}local-public.ip")
+output "SSH-key-name" {
+  value = local.ssh_key_name
 }
 
 
