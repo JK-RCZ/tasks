eaec6cc0a1918186b8b626991b3d0b4a56e3a18d
JK-RCZ <jankarchewski@gmail.com>
2023-09-15 15:50:44 +0300

    nginx backup script finished

--- scripts/nginx.backup.sh
+++ scripts/nginx.backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script backups nginx.conf each day and stores this backups for 7 days or if there's more then 7 backups stored
+# This script backups nginx configuration files each day and stores this backups in user picked folder for 7 days or if there's more then 7 backups stored
 
 function root_check { # checks if user is root
     if [ "$(whoami)" != root ]; then
@@ -22,7 +22,7 @@ function current_user_name { # checks name of current user
 
 function create_crontab_job { # creates crontab job that launches this script
     cp cron.conf "${user_name}"
-    echo "* * * * * ${script_path}" >> "${user_name}"
+    echo "${cron_interval} ${script_path}" >> "${user_name}"
     cp "${user_name}" /var/spool/cron/crontabs/
     chown "${user_name}":crontab /var/spool/cron/crontabs/"${user_name}"
     chmod 600 /var/spool/cron/crontabs/"${user_name}"
@@ -38,29 +38,60 @@ function create_nginx_backup { # creates archive file with nginx config data
 }
 
 function ask_for_directory { # asks user for backup files directory and creates it
-    echo "Please enter the folder you would like to save nginx backups:"
+    echo -e "\nPlease enter the folder you would like to save nginx backups:"
     read -r backup_folder
     if test -d "${backup_folder}";
         then
-            chown -R "${user_name}":crontab "${backup_folder}"
+            chown -R "${user_name}":"${user_name}" "${backup_folder}"
             touch backup_path.txt
             echo "${backup_folder}" > backup_path.txt
         else
             mkdir "${backup_folder}"
-            chown -R "${user_name}":crontab "${backup_folder}"
+            chown -R "${user_name}":"${user_name}" "${backup_folder}"
             touch backup_path.txt
             echo "${backup_folder}" > backup_path.txt
     fi
 }
 
+function test_or_not { # asks for testmode (backups created every minute)
+    echo -e "\nHello, ""${user_name^}""!\nThis script is for creating nginx configuration files backup once a day."
+    echo -e "Would you like to test backup script first (backups will be created every minute)? Y/N"
+    read -r test_mode
+    if [ "${test_mode}" == "y" ];
+        then
+            cron_interval="* * * * *"
+            
+        else
+            cron_interval="1 1 * * 0-6"
+            
+    fi
+}
+
+function delete_older_then_seven { # deletes files older then 7 days or if their amount is more the 7
+    
+    find "${backup_path}" -type f -mtime +7 -name '*.gz' -execdir rm -- '{}' \; #deletes files older then 7 days
+
+    count_files="$(ls -l "${backup_path}"/*.gz | wc -l)" #counts files in folder
+    del_quantity="$((count_files-7))" # amount of files to delete
+    if [ "${count_files}" -gt "7" ] # deletes files if their amount is more the 7
+        then
+            for i in $(ls -l "${backup_path}"/*.gz | head -n "${del_quantity}")
+            do
+                    rm "${i}"
+            done
+    fi
+}
+
 if [ -t 0 ]; # checks if script was launched by user or by daemon
-    then
+    then # if user
         root_check
         current_user_name
+        test_or_not
         ask_for_directory        
         current_script_path
         create_crontab_job
-    else
+    else # if daemon
         current_script_path
         create_nginx_backup
+        delete_older_then_seven
 fi
