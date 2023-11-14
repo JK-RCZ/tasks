d2df441fe1a67d231662cda5dcd3cd6fd1037b00
JK-RCZ <jankarchewski@gmail.com>
2023-11-14 15:02:50 +0300

    update

--- terraform/aws-el-gato-project-v0.2/scripts/change-db-endpoint.sh
+++ terraform/aws-el-gato-project-v0.2/scripts/change-db-endpoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-db_endpoint=$(cat ../db_endpoint.txt)
-path_to_docker_compose_file="../docker/LAMP/docker-compose.yml"
+db_endpoint=$(cat /tasks/terraform/aws-el-gato-project-v0.2/db_endpoint.txt)
+path_to_docker_compose_file="/tasks/terraform/aws-el-gato-project-v0.2/docker/LAMP/docker-compose.yml"
 
-echo "${db_endpoint}"
+
 sed -i "s|- PMA_HOST=.*$|- PMA_HOST=${db_endpoint}|" ${path_to_docker_compose_file}
