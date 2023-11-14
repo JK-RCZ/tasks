aff15a35f3500464f79f2f2902c2397a50f25bee
JK-RCZ <jankarchewski@gmail.com>
2023-11-14 16:18:54 +0300

    update

--- terraform/aws-el-gato-project-v0.2/scripts/change-db-endpoint.sh
+++ terraform/aws-el-gato-project-v0.2/scripts/change-db-endpoint.sh
@@ -5,3 +5,4 @@ path_to_docker_compose_file="/tasks/terraform/aws-el-gato-project-v0.2/docker/LA
 
 
 sed -i "s|- PMA_HOST=.*$|- PMA_HOST=${db_endpoint}|" ${path_to_docker_compose_file}
+rm /tasks/terraform/aws-el-gato-project-v0.2/db_endpoint.txt
--- terraform/aws-el-gato-project-v0.2/scripts/install-LAMP.sh
+++ terraform/aws-el-gato-project-v0.2/scripts/install-LAMP.sh
@@ -12,13 +12,13 @@
 # FOR SUSE------------------------------------------
 
 
-sudo yum update
-sudo yum install -y git
-sudo yum install -y docker
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo systemctl start docker
+yum update
+yum install -y git
+yum install -y docker
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+systemctl start docker
 git clone https://github.com/JK-RCZ/tasks.git
 /usr/bin/bash  /tasks/terraform/aws-el-gato-project-v0.2/scripts/change-db-endpoint.sh
 cd /tasks/terraform/aws-el-gato-project-v0.2/docker/LAMP/
-sudo docker-compose up -d
+docker-compose up -d
