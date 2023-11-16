d515ded5849aa87309ff755b670307de50e75fc5
JK-RCZ <jankarchewski@gmail.com>
2023-11-16 11:21:19 +0300

    update

--- terraform/modules/aws-lb-listener-rule/lb-listener-rule.tf
+++ terraform/modules/aws-lb-listener-rule/lb-listener-rule.tf
@@ -2,10 +2,12 @@
 # Please set respective dependensies in root module!
 
 data "aws_lb_listener" "data" {
+    load_balancer_arn = data.aws_lb.data.arn
+    port = data.aws_lb_target_group.data.port
     tags             = {
       Name           = var.listener_rule.environment_parameters.listener_name
     }
-  
+  depends_on = [ data.aws_lb.data, data.aws_lb_target_group.data ]
 }
 
 data "aws_lb_target_group" "data" {
