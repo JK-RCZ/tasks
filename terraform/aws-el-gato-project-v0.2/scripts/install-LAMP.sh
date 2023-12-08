71a1ca2c720b210318ff6d80ba46d348a25dce5a
JK-RCZ <jankarchewski@gmail.com>
2023-12-08 17:38:34 +0300

    update

--- terraform/aws-el-gato-project-v0.2/scripts/install-LAMP.sh
+++ terraform/aws-el-gato-project-v0.2/scripts/install-LAMP.sh
@@ -15,6 +15,6 @@ chmod +x /usr/local/bin/docker-compose
 systemctl start docker
 git clone --branch staging https://github.com/JK-RCZ/tasks.git
 #usr/bin/bash  /tasks/terraform/aws-el-gato-project-v0.2/scripts/change-config.sh
-rm /tmp/db_data.txt
+#rm /tmp/db_data.txt
 cd /tasks/terraform/aws-el-gato-project-v0.2/docker/LAMP/test/ #delete "/test"
 docker-compose up -d
