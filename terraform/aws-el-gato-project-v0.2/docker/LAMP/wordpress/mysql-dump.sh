657a818f76801cfb598e12509ea36c4d21599387
JK-RCZ <jankarchewski@gmail.com>
2023-12-13 19:08:24 +0300

    update

--- terraform/aws-el-gato-project-v0.2/docker/LAMP/wordpress/mysql-dump.sh
+++ terraform/aws-el-gato-project-v0.2/docker/LAMP/wordpress/mysql-dump.sh
@@ -8,3 +8,4 @@ db_name="wordpress"
 current_date=$(date +"%d-%m-%Y_%T" | awk -F: '{ print ($1)"h"($2)"m"($3)"s" }')
 mkdir -p /mariadb-backup
 mysqldump -u $user_name --password=$password -h $db_host $db_name > /mariadb-backup/$db_name-$current_date.sql 
+ 
--- terraform/aws-el-gato-project-v0.2/scripts/push-to-s3.sh
+++ terraform/aws-el-gato-project-v0.2/scripts/push-to-s3.sh
@@ -3,5 +3,6 @@ sudo mkdir -p /wordpress-log/
 sudo mkdir -p /mariadb-backup/
 sudo docker cp wordpress-webserver:/var/www/localhost/htdocs/wp-content/debug.log /wordpress-log/
 sudo docker cp wordpress-webserver:/mariadb-backup/. /mariadb-backup/
-aws s3 cp --recursive /mariadb-backup/ s3://lil-pretty-bucket/mariadb-backup/
+tar -czf mariadb-backups.tar.gz --absolute-names /mariadb-backup/
+aws s3 mv --recursive /mariadb-backup/mariadb-backups.tar.gz s3://lil-pretty-bucket/mariadb-backup/
 aws s3 cp --recursive /wordpress-log/ s3://lil-pretty-bucket/wordpress-log/
