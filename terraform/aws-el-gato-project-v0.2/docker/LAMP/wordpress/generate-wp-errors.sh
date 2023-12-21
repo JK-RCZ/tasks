47cdd02ba6bb8c59c054e9db7f197bef27874a4d
JK-RCZ <jankarchewski@gmail.com>
2023-12-21 15:48:01 +0300

    update

--- terraform/aws-el-gato-project-v0.2/docker/LAMP/wordpress/generate-wp-errors.sh
+++ terraform/aws-el-gato-project-v0.2/docker/LAMP/wordpress/generate-wp-errors.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#This script causes momentary errors in Wordpress theme twentytwentyfour
+
 sed -i "s|if ( ! function_exists( 'twentytwentyfour_block_styles' ) ) :|if ( ! function_exists( 'twentytwentyfour_block_styles' ) ) ;|" /var/www/localhost/htdocs/wp-content/themes/twentytwentyfour/functions.php
 curl localhost
 sed -i "s|if ( ! function_exists( 'twentytwentyfour_block_styles' ) ) ;|if ( ! function_exists( 'twentytwentyfour_block_styles' ) ) :|" /var/www/localhost/htdocs/wp-content/themes/twentytwentyfour/functions.php
--- terraform/aws-el-gato-project-v0.2/docker/LAMP/wordpress/mysql-dump.sh
+++ terraform/aws-el-gato-project-v0.2/docker/LAMP/wordpress/mysql-dump.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 db_host="db"
-user_name="henry"
-password="any"
+db_user="henry"
+db_pass="any"
 db_name="wordpress"
 
 current_date=$(date +"%d-%m-%Y_%T" | awk -F: '{ print ($1)"h"($2)"m"($3)"s" }')
 mkdir -p /mariadb-backup
-mysqldump -u $user_name --password=$password -h $db_host $db_name > /mariadb-backup/$db_name-$current_date.sql 
+mysqldump -u $db_user --password=$dbpass -h $db_host $db_name > /mariadb-backup/$db_name-$current_date.sql 
  
