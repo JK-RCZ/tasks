10aa04477eaafa03a5c0dcc2cd4b3a4f823c457f
JK-RCZ <jankarchewski@gmail.com>
2024-01-26 10:21:40 +0300

    s3 output update

--- terraform/modules/aws-s3-bucket/outputs.tf
+++ terraform/modules/aws-s3-bucket/outputs.tf
@@ -1,4 +1,7 @@
-
-output "s3_id" {
-    value = aws_s3_bucket.this.id  
+output "s3_data" {
+    description               = "s3 main data"
+    value                     = {
+        name                  = aws_s3_bucket.this.tags.Name,
+        id                    = aws_s3_bucket.this.id
+    }
 }
